b9c2bfa6f5f3dbca9ac653ab3527c4c56697af46
Nikita Kondrashov <nickita.kondraschov@yandex.ru>
2021-03-01 21:16:55 +0300

    change

--- task1/app/settings.py
+++ task1/app/settings.py
@@ -100,7 +100,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+RUS_INFL = os.path.join(BASE_DIR, 'inflation_russia.csv')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
--- task1/app/views.py
+++ task1/app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.conf import settings
+import csv
 
 def inflation_view(request):
     template_name = 'inflation.html'
+    with open (settings.RUS_INFL, encoding = 'utf-8') as f:
+        f_read = csv.reader(f)
+        infl = list(f_read)
+        # print(list(f_read))
 
     # чтение csv-файла и заполнение контекста
-    context = {}
+    context = {'infl_list':infl}
 
     return render(request, template_name,
                   context)
