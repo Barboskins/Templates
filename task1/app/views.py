d9b8a808627e607809848ce75232a44b77766565
Nikita Kondrashov <nickita.kondraschov@yandex.ru>
2021-03-03 20:02:36 +0300

    03.03.21 new commit

--- task1/app/views.py
+++ task1/app/views.py
@@ -6,9 +6,7 @@ def inflation_view(request):
     template_name = 'inflation.html'
     with open (settings.RUS_INFL, encoding = 'utf-8') as f:
         f_read = csv.reader(f,delimiter = ';')
-        # f_read = csv.DictReader(f, delimiter = ';')
         infl = list(f_read)
-        print(infl)
 
     # чтение csv-файла и заполнение контекста
     context = {'infl_list':infl, 'empty': '-'}
